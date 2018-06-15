745de276dc5da8769ce4c2df02017615b6b26fc3
sdana <seth.dana@gmail.com>
2018-06-15 11:40:19 -0500

    Input fields clear on focus now, added color and some more styles

--- src/scripts/contactForm.js
+++ src/scripts/contactForm.js
@@ -3,14 +3,17 @@ const buildContactInputSection = () => {
     nameInputField.id = "name"
     nameInputField.class = "inputField"
     nameInputField.value = "Name"
+    nameInputField.setAttribute("onfocus", "this.value=''")
     const addressInputField = document.createElement("input")
     addressInputField.id = "address"
     addressInputField.class = "inputField"
     addressInputField.value = "Address"
+    addressInputField.setAttribute("onfocus", "this.value=''")
     const phoneNumberInputField = document.createElement("input")
     phoneNumberInputField.id = "phoneNumber"
     phoneNumberInputField.class = "inputField"
     phoneNumberInputField.value = "Phone Number"
+    phoneNumberInputField.setAttribute("onfocus", "this.value=''")
     const submitButton = document.createElement("button")
     submitButton.id = "submitButton"
     submitButton.setAttribute("form", "#firstName #lastName #address #phoneNumber")
